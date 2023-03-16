8a46c36ec17778b4609bbee7bd87e3e0cb7d29fb
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-03-16 22:33:14 +0900

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,90 @@
+const startChatBtn = document.getElementById('startChat');
+const stopChatBtn = document.getElementById('stopChat');
+const chatArea = document.getElementById('chatArea');
+const messages = document.getElementById('messages');
+const messageInput = document.getElementById('messageInput');
+const sendMessageBtn = document.getElementById('sendMessage');
+let socket = null;
+
+startChatBtn.addEventListener('click', () => {
+  const roomId = document.getElementById('roomId').value.trim();
+  console.log(roomId)
+  if (roomId) {
+    startChat(roomId);
+  }
+});
+
+stopChatBtn.addEventListener('click', () => {
+  stopChat();
+});
+
+sendMessageBtn.addEventListener('click', () => {
+  sendMessage();
+});
+
+messageInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    sendMessage();
+  }
+});
+
+function startChat(roomId) {
+  socket = new WebSocket('ws://127.0.0.1:5500');
+
+  socket.addEventListener('open', () => {
+    onSocketOpen();
+    joinRoom(roomId);
+  });
+  socket.addEventListener('message', onSocketMessage);
+  socket.addEventListener('close', onSocketClose);
+  socket.addEventListener('error', onSocketError);
+
+  startChatBtn.disabled = true;
+  stopChatBtn.disabled = false;
+}
+
+function stopChat() {
+  if (socket) {
+    socket.close();
+  }
+
+  startChatBtn.disabled = false;
+  stopChatBtn.disabled = true;
+  chatArea.style.display = 'none';
+  messages.innerHTML = '';
+}
+
+function joinRoom(roomId) {
+  if (socket) {
+    socket.send(JSON.stringify({ type: 'join', roomId: roomId }));
+  }
+}
+
+function sendMessage() {
+    const message = messageInput.value.trim();
+    if (message && socket) {
+      socket.send(JSON.stringify({ type: 'message', content: message }));
+      messageInput.value = '';
+    }
+  }
+  
+
+function onSocketOpen() {
+  chatArea.style.display = 'block';
+}
+
+function onSocketMessage(event) {
+  const message = event.data;
+  const messageElement = document.createElement('div');
+  messageElement.textContent = message;
+  messages.appendChild(messageElement);
+}
+
+function onSocketClose(event) {
+  stopChat();
+}
+
+function onSocketError(event) {
+  console.error('WebSocket error:', event);
+}
